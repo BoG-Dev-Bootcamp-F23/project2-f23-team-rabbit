0b18cafc74d4c49a20421cff6119b7a5180a3624
snehayourpal <snehapal205@gmail.com>
2023-11-16 19:25:42 -0500

    Completed Update Operation

--- server/mongodb/actions/updateHoursTrained.js
+++ server/mongodb/actions/updateHoursTrained.js
@@ -1,14 +1,28 @@
 import { connectDB } from '../index.js';
 import Animal from "../models/Animal.js"
 
-export const updateAnimalHoursTrained = async (animalId, hoursTrained) => {
-    try {
-      const { Animal } = await connectDB();
-      await Animal.updateOne({ _id: animalId }, { $inc: { hoursTrained } });
-  
-      return { success: true };
-    } catch (error) {
-      console.error(error);
-      return { success: false, error: 'Internal Server Error' };
+export default async function updateAnimalHoursTrained(animalId, hoursTrained) {
+  try {
+    await connectDB();
+
+    const existingAnimal = await Animal.findById(animalId);
+
+    if (!existingAnimal) {
+      return { success: false, error: 'Animal not found' };
     }
-  };
+
+    const updatedAnimal = await Animal.updateOne(
+      { _id: animalId }, 
+      { $inc: { hoursTrained } }
+    ); 
+    
+    if (updatedAnimal) {
+      return { success: true };
+    } else {
+      return { success: false, error: 'Update failed' };
+    }
+  } catch (error) {
+    console.error(error);
+    return { success: false, error: 'Internal Server Error' };
+  }
+};
