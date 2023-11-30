bf680e1eeee12c5156e7d7300b5f7a03b4805e2e
yolanda-y-li <yolandayli1600@gmail.com>
2023-11-30 12:39:21 -0500

    add unstyles training log header

--- src/components/TrainingLogs.js
+++ src/components/TrainingLogs.js
@@ -1,7 +1,14 @@
 import TrainingLogCard from "./TrainingLogCard";
+import styles from "@/styles/TrainingLogs.module.css";
 
 export default function TrainingLogs() {
     return (
+        <>
+        <div className={styles.header}>
+            <h1>Training Logs</h1>
+            <button className= {styles.createLogButton}>+ Create Log</button>
+        </div>
         <TrainingLogCard />
+        </>
     )
 }
